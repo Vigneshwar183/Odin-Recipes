fbba639f3ca064be95f35d578f107f09754615e1
Vigneshwar183 <avigneshwar98@gmail.com>
2022-03-02 17:16:17 +0530

    Final output

--- etch-a-sketch/eas.js
+++ etch-a-sketch/eas.js
@@ -1,28 +1,51 @@
 function createGrid(size){
     parent=document.getElementsByClassName('parent-div');
-    console.log(parent[0]);
-    let height=Math.floor(vh/size);
+    let height=Math.floor(900/size);
     for(let i=0;i<size;i++){
         for(let j=0;j<size;j++){
             div=document.createElement('div');
             div.className='grid';
             div.style.height=height+'px';
             div.style.width=height+'px';
+            div.style.margin=0;
             parent[0].appendChild(div);
         }
     }
 }
 
+function clear(){
+    let elements=document.querySelectorAll('.grid');
+    
+    if (elements!==null){    
+        elements.forEach(element=>{
+            element.parentNode.removeChild(element);
+            
+        })
+        console.log('hi');
+    }
+}
+
+function alterDiv(){
+    let gridBoxes=document.querySelectorAll('.grid');
+    console.log(gridBoxes);
+    gridBoxes.forEach(element =>{
+        element.addEventListener('mouseover',(event)=>{
+            event.target.style.backgroundColor='black';
+        })
+    })
+}
+
+
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 let button=document.querySelector('.btn');
 button.addEventListener('click',()=>{
-    let div=document.querySelectorAll('grid');
-    if(div !==null){    
-        div.forEach(element => {
-                div.remove();
-        });
+    if(document.querySelector('.grid')===null){
+        let size=+prompt(`Enter grid size`);
+        createGrid(size);
+        alterDiv();
+    }else{
+        clear();
     }
-    let size=+prompt(`Enter grid size`);
-    createGrid(size);
 })
+
