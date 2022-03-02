3ef9bd6ae86bd5a0c2c76fc29e3df4d3f1cda56c
Vigneshwar183 <avigneshwar98@gmail.com>
2022-03-02 21:58:27 +0530

    Working model

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -18,6 +18,11 @@ function divide(a,b){
     }
 }
 
+function display_output(){
+    parent=document.querySelector('.display');
+    parent.textContent=output;
+}
+
 function assign(decimal_active,event){    
     if (decimal_active===0){    
         temporary=temporary*10 + +event.target.value;
@@ -26,15 +31,15 @@ function assign(decimal_active,event){
         temporary=temporary+ temp;
         decimal_place*=10;
     }
-    if (operator_active===''){
+    if (operator_active==='' || operator_active==='='){
         firstNumber=temporary;
         output=temporary;
-    }
-    else{
+    }else{
         secondNumber=temporary;
         output=firstNumber+operator_active+secondNumber;
     }
     console.log(output);
+    display_output();
 }
 
 let firstNumber=null;
@@ -44,6 +49,7 @@ let decimal=document.querySelector('.decimal');
 let operator=document.querySelectorAll('.operator');
 let decimal_active=0;
 let operator_active='';
+let operator_in_use=''
 let temporary=0;
 let output='';
 
@@ -58,6 +64,7 @@ decimal.addEventListener('click',(event)=>{
             output=firstNumber+operator_active+secondNumber+'.';
         }
         console.log(output);
+        display_output();
     }else{
         decimal_active=2;
     }
@@ -65,18 +72,58 @@ decimal.addEventListener('click',(event)=>{
 
 digit_button.forEach(element=>{
     element.addEventListener('click',(event)=>{
-        assign(decimal_active,event);        
+        if(event.target.value!=='='){
+            assign(decimal_active,event);
+        }        
     })
 })
 
 operator.forEach(element=>{
     element.addEventListener('click',(event)=>{
-        if (firstNumber!==null){
+        if(firstNumber!==null && secondNumber!==null){
+            operator_active=event.target.value;
+            switch(operator_in_use){
+                case '+':{
+                    firstNumber=add(firstNumber,secondNumber);
+                    temporary=0;
+                    secondNumber=null;
+                    break;
+                }
+                case '-':{
+                    firstNumber=subtract(firstNumber,secondNumber);
+                    temporary=0;
+                    secondNumber=null;
+                    break;
+
+                }
+                case '*':{
+                    firstNumber=multiply(firstNumber,secondNumber);
+                    temporary=0;
+                    secondNumber=null;
+                    break;
+                }
+                case '/':{
+                    firstNumber=divide(firstNumber,secondNumber);
+                    temporary=0;
+                    secondNumber=null;
+                    break;
+                }
+            }
+            if (operator_active !=='='){
+                output=firstNumber+operator_active;
+            }else{
+                output=firstNumber;
+            }
+            console.log(output);
+            display_output();
+        }else if (firstNumber!==null){
             operator_active=event.target.value
             decimal_active=0;
+            operator_in_use=event.target.value
             temporary=0;
             console.log(firstNumber+operator_active);
             output=firstNumber+operator_active;
+            display_output();
         }
 
     })
