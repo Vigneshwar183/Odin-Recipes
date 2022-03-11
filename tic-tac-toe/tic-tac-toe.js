96cf7bc49be8e4c6f4d3d29c0b422e66aec7344d
Vigneshwar183 <avigneshwar98@gmail.com>
2022-03-11 22:25:17 +0530

    final game

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,142 +1,149 @@
 const gameBoard=(()=>{
-    var gameboard=[['1','1','1'],['1','1','1'],['1','1','1']];
-    let count=0;
-    var users=(player1,player2)=>{
-        var user1=player1;
-        var user2=player2;
-        console.log(user1,user2);
-        turn(count,user1,user2);
-    }
     var turn=(count,user1,user2)=>{
-        if (count%2===0){
-            display(user1);
-            checkResult(user1);
-            count+=1;
-        }
-        else{
-            display(user2);
-            checkResult(user2);
-            count+=1;
-        }            
+        console.log('turn called');
+        let result;
+        let positions=document.getElementsByTagName('td');
+        for (var i=0;i<positions.length;i++){
+            positions[i].addEventListener('click',(event)=>{
+                if (count%2===0){
+                    updateGameboard(user1,event.target.id)
+                    result=checkResult(user1,event.target.id);
+                    count+=1;
+                }
+                else{
+                    updateGameboard(user2,event.target.id);
+                    result=checkResult(user2,event.target.id);
+                    count+=1;
+                } 
+                if (count===9){
+                    console.log('tie');
+                    return
+                }
+                if (result===true){
+                    if (count%2!==0){
+                        console.log(`${user1.name} wins`);
+                        user1.score+=1;
+                        let temp=document.querySelector('body');
+                        let tempChild=document.createElement('div');
+                        tempChild.innerHTML=`${user1.name}:${user1.score} || ${user2.name}:${user2.score}`;
+                        temp.appendChild(tempChild);
+                        return
+                    }
+                    else{
+                        console.log(`${user2.name} wins`);
+                        user2.score+=1;
+                        let temp=document.querySelector('body');
+                        let tempChild=document.createElement('div');
+                        tempChild.innerHTML=`${user1.name}:${user1.score} || ${user2.name}:${user2.score}`;
+                        temp.appendChild(tempChild);
+                        return
+                    }
+
+                }
+            })
+        } 
+                   
     }
     var updateGameboard=(user,position)=>{
         row=+position[0];
         column=+position[2];
+        console.log(typeof gameboard[row][column]);
         if (gameboard[row][column]==='1'){
             gameboard[row][column]=user.symbol;
-            console.log(gameboard);
             count+=1;
-            turn(count);
         }
         else{
             alert('Invalid Move');
         }
+        let changePosition=document.getElementById(position);
+        changePosition.innerHTML=user.symbol;
+        return null
     }
-    var display=(user)=>{
-        console.log(user);
-        let parent=document.querySelector('body');
-        for (var i=0;i<gameboard.length;i++){
-            let tr=document.createElement('tr');
-            for(var j=0;j<gameboard[0].length;j++){
-                let td=document.createElement('td');
-                td.innerHTML=gameboard[i][j];
-                td.setAttribute('id',i+'+'+j);
-                td.style.padding='5px';
-                tr.appendChild(td);
+    var checkResult=(user,position)=>{
+        let row=+position[0];
+        let col=+position[2];
+        if (gameboard[0][0]===gameboard[1][1] && gameboard[0][0]===gameboard[2][2]){
+            if (gameboard[0][0]!=='1'){
+                console.log('hi');
+                return true
             }
-            parent.appendChild(tr);
         }
-        let positions=document.getElementsByTagName('td');
-        for (var i=0;i<positions.length;i++){
-            positions[i].addEventListener('click',(event)=>{
-                updateGameboard(user,event.target.id)
-            })
+        if (gameboard[1][1]===gameboard[0][2] && gameboard[1][1]===gameboard[2][0]){
+            if (gameboard[1][1]!=='1'){
+                console.log('hi2');
+                return true
+            }
         }
+        if (gameboard[0][col]===gameboard[1][col] && gameboard[0][col]===gameboard[2][col]){
+            if (gameboard[0][col]!=='1'){
+                console.log('hi3');
+                return true
+            }
+        }
+        if (gameboard[row][0]===gameboard[row][1] && gameboard[row][0]===gameboard[row][2]){
+            if (gameboard[row][0]!=='1'){
+                console.log('hi4');
+                return true
+            }
+        }
+        return false
     }
-    var checkResult=(user)=>{
-        let count=0;
-        flag=0;
-        let temp=gameboard[0][0];
-        if (flag===0)
-        {
-            for (var i=0,j=0;i<gameboard.length;i++,j++){
-                if (gameboard[i][j]===temp)
-                    count+=1;
-            }
+    var reset=()=>{
+        count=0;
+        let temp2=document.getElementsByTagName('td');
+        for (let j=0;j<temp2.length;j++){
+            temp2[j].innerHTML='1';
         }
-        if (count==3){
-            flag=1;
-            winnerSymbol=temp;
-        }
-        temp=gameboard[0][0];
-        if (flag===0){
-            for (var i=0;i<gameboard.length;i++){
-                let temp=gameboard[i][0];
-                count=0;
-                for (var j=0;j<gameboard.length;j++){
-                    if (gameboard[i][j]===temp)
-                        count+=1
-                }
-                if (count===3){
-                    flag=1;
-                    break;
-                }
-            }
-        }
-        if (count==3){
-            flag=1;
-            winnerSymbol=temp;
-        }
-        temp=gameboard[0][0];
-        if (flag===0){
-            for (var i=0;i<gameboard.length;i++){
-                let temp=gameboard[i][0];
-                count=0;
-                for (var j=0;j<gameboard.length;j++){
-                    if (gameboard[j][i]===temp)
-                        count+=1
-                }
-                if (count===3){
-                    flag=1;
-                    break;
-                }
-            }
-        }
-        if (count==3){
-            flag=1;
-            winnerSymbol=temp;
-        }
-        if (flag===0)
-            console.log('tie');
+        console.log(count);
+        return
+    }
 
-    }
-    return {turn,display,users}
+    return {gameboard,turn,reset}
 })();
 
-const Display=()=>{
+const mainDisplay=((gameboard)=>{
+    let parent=document.querySelector('body');
+    let table=document.createElement('table');
+    for (var i=0;i<gameboard.length;i++){
+        let tr=document.createElement('tr');
+        for(var j=0;j<gameboard[0].length;j++){
+            let td=document.createElement('td');
+            td.innerHTML=gameboard[i][j];
+            td.setAttribute('id',i+'+'+j);
+            td.style.padding='5px';
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    parent.appendChild(table);
+})
 
-}
-
-const player=(name,symbol)=>{
-    return {name,symbol}
+const player=(name,symbol,score)=>{
+    return {name,symbol,score}
 }
 
 let symbol1='X';
 let symbol2='O';
-// let form=document.getElementsByTagName('form');
+var gameboard=[['1','1','1'],['1','1','1'],['1','1','1']];
+let count=0;
+let user1;
+let user2;
 let form=document.querySelector('form');
 form.onsubmit=(event)=>{
     event.preventDefault();
     console.log(document.getElementById('name1').value);
-    let player1= player(document.getElementById('name1').value,symbol1);
-    let player2= player(document.getElementById('name2').value,symbol2);
-    console.log(player1,player2);
-    gameBoard.users(player1,player2)
+    let player1= player(document.getElementById('name1').value,symbol1,0);
+    let player2= player(document.getElementById('name2').value,symbol2,0);
+    user1=player1;
+    user2=player2;
+    mainDisplay(gameboard);
+    gameBoard.turn(0,user1,user2);
 }
-
-// let positions=document.getElementsByTagName('td');
-// for (var i=0;i<positions.length;i++){
-//     positions[i].addEventListener('click',(event)=>{
-//         event.target.innerHTML='hi';
-//     })
-// }
+let reset=document.getElementById('reset');
+reset.onclick=()=>{
+    let table=document.getElementsByTagName('table');
+    table[0].remove();
+    gameboard=[['1','1','1'],['1','1','1'],['1','1','1']];
+    mainDisplay(gameboard);
+    gameBoard.turn(0,user1,user2);
+}
